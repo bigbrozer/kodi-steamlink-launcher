fd9c0822dcb6673db35ef95d008fcc18fe4587f6
hawk291 <46007903+hawk291@users.noreply.github.com>
2023-08-05 19:26:47 +0000

    Update start.sh
    set both scripts as executable, tested before this change and steamlink didn't start, but after setting the execute bit after reboot steamlink starts

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -2,6 +2,7 @@
 
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
+cd ${DIRNAME} && sudo chmod u+x heartbeat.sh && sudo chmod u+x start.sh
 sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
 sudo systemctl stop kodi
 
